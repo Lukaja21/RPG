613161f59e3eba008c1791d2c3496683c33d9c06
Kai Chang <kaijchang@gmail.com>
2018-08-13 15:36:00 -0700

    start on app

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template
-import generators
+from flask import Flask, render_template, request
+from generators import (loot_generator, shop_generator,
+                        npc_generator, quest_generator)
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return 'Hello World!'
+
+
+@app.route('/quest_generator')
+def quest_page():
+    if request.args['quest_type'].lower() in ['gather', 'battle']:
+        return render_template('quest_page.html', quest=quest_generator(request.args['quest_type']))
+
+    else:
+        return 'Error'
+
 
 app.run()
